e2f31f6ea6818da68552110ca6e558e1e1ceaca0
yujoonhan <sick1925@gmail.com>
2021-08-27 14:38:18 +0900

    프로젝드 생성

---
+++ joonAqua/src/main/java/com/greenart/JoonAquaApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonAquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoonAquaApplication.class, args);
+	}
+
+}
